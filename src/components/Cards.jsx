123ba0446d24f3fa41395be9bbb9149d77565ea9
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2024-05-19 01:17:41 -0400

    triple users changed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -34,7 +34,7 @@ const Cards = () => {
             <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8'>Triple Users</h2>
-                <p className='text-center text-4xl font-bold'>$379</p>
+                <p className='text-center text-4xl font-bold'>$389</p>
                 <div className='text-center font-medium'>
                     <p className='py-2 border-b mx-8 mt-8'>5 TB Storage</p>
                     <p className='py-2 border-b mx-8'>3 Granted Users</p>

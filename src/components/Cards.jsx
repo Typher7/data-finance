a3d1e9d80273879b13ac47ec05a88f24b159e68c
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2024-05-19 01:01:57 -0400

    card details corrected for double and single

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -22,11 +22,11 @@ const Cards = () => {
             <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-4 rounded-lg hover:scale-105 duration-300'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8'>Double Users</h2>
-                <p className='text-center text-4xl font-bold'>$149</p>
+                <p className='text-center text-4xl font-bold'>$249</p>
                 <div className='text-center font-medium'>
-                    <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                    <p className='py-2 border-b mx-8'>1 Granted User</p>
-                    <p className='py-2 border-b mx-8'>Send up to 2GB</p>
+                    <p className='py-2 border-b mx-8 mt-8'>2 TB Storage</p>
+                    <p className='py-2 border-b mx-8'>2 Granted Users</p>
+                    <p className='py-2 border-b mx-8'>Send up to 5GB</p>
                 </div>
                 <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
 
@@ -34,11 +34,11 @@ const Cards = () => {
             <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8'>Triple Users</h2>
-                <p className='text-center text-4xl font-bold'>$149</p>
+                <p className='text-center text-4xl font-bold'>$399</p>
                 <div className='text-center font-medium'>
-                    <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                    <p className='py-2 border-b mx-8'>1 Granted User</p>
-                    <p className='py-2 border-b mx-8'>Send up to 2GB</p>
+                    <p className='py-2 border-b mx-8 mt-8'>5 TB Storage</p>
+                    <p className='py-2 border-b mx-8'>3 Granted Users</p>
+                    <p className='py-2 border-b mx-8'>Send up to 10GB</p>
                 </div>
                 <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Start Trial</button>
 
